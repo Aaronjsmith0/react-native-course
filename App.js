6fac89b01e27b12798a3d952f435506b1be9da8f
Aaron Smith <aaron.smith@sgws.com>
2020-02-19 21:17:38 -0600

    saving some styling and input box and button

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: "space-between", alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" style={{ borderColor: 'red', borderWidth: 1, padding: 10, width: '80%' }} />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
